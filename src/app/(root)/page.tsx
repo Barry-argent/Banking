8bda60093148f9aff889eccffd4e04e0c05d0a27
Barry1003 <barryargent1003@gmail.com>
2024-05-19 12:46:36 +0100

    update

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,20 @@
+import HeaderBox from "@/components/ui/HeaderBox"
 
 const Home = () => {
+  const loggedIn = {firstName: 'Usaamah'}
   return (
-    <div>
-         ome
-    </div>
+    <section className='home'>
+        <div className="home-content">
+          <header className="home-header">
+               <HeaderBox
+                type="greeting"
+                title="welcome"
+                user= {loggedIn?.firstName || 'guest'}
+                subtext = "Access and mange your account and transaction efficiently."
+               />
+          </header>
+        </div>
+    </section>
   )
 }
 
